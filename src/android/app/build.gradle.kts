3f43ff7b55888e914eae80801bf1ae7d02f31169
Kleidis <kleidisn@hotmail.com>
2024-03-28 13:40:01 +0100

    Revert "make more simple to get cnsdk version name"
    This reverts commit 527081028545480e0a495a6bdc4da647a9421a78.

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -55,10 +55,7 @@ val cnsdkPath: String = properties.getProperty("cnsdk.dir").also {
 }
 println("CNSDK directory: $cnsdkPath")
 val getCNSDKVersionName: () -> String = {
-    // Extract the file name from the path
-    val fileName = cnsdkPath.substringAfterLast("/")
-    // Remove the prefix from the file name to get the version
-    fileName.removePrefix("cnsdk-android-")
+    file(File(cnsdkPath, "VERSION.txt")).readText()
 }
 fun getCNSDKLibName(forceInApp: Boolean): String {
     return if (useInAppFaceTracking() || forceInApp) {
