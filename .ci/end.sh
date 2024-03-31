a1c3a6986db0955b5a431d163da957b7902257fc
Gamer64ytb <76565986+Gamer64ytb@users.noreply.github.com>
2024-03-31 12:54:23 +0200

    re-work ci

--- .ci/end.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-ccache -s -v
-
-if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
-    chmod +x /home/runner/work/Lemonade/Lemonade/src/android/app/ks.jks
-    rm "/home/runner/work/Lemonade/Lemonade/src/android/app/ks.jks"
-fi
